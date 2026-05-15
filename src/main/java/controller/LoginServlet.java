546be185a301f02b2ca8987d2794722dea138379
sage0422 <hiromusage@gmail.com>
2026-05-16 00:46:08 +0900

    aa

--- src/main/java/controller/LoginServlet.java
+++ src/main/java/controller/LoginServlet.java
@@ -27,7 +27,7 @@ public class LoginServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/login.jsp");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
 		dispatcher.forward(request, response);
 
 	}
